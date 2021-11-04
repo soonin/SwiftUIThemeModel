5b561930f093c895a4a2f908a23ba10ab67de338
pooya <info@centurytrail.com>
2021-11-04 01:33:18 -0700

    Initial Commit

---
+++ SwiftUIThemeModel/SwiftUIThemeModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIThemeModelApp.swift
+//  SwiftUIThemeModel
+//
+//  Created by Pooya on 2021-11-04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIThemeModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
